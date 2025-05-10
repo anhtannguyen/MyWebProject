d78314f4958c452d471b9cfef9318751ecfb4030
NguyenAnhTan <93763244+anhtannguyen@users.noreply.github.com>
2025-05-10 23:54:22 +0700

    Update script.js
    Signed-off-by: NguyenAnhTan <93763244+anhtannguyen@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function openProject(url) {
     window.location.href = url;
 }
+//bấm vào link và nhảy sang trang web hiện tại//
+function openProject(url) {
+    window.open(url, '_blank');
+}
+// Bấm vào link và mở 1 trang web mới cùng trang chủ // áp dụng nguyên tắc cái nào có trước thì sẽ áp dụng trước //
