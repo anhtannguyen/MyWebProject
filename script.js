95a6db281940222fd35709abdfce9d5eeb39904d
Nguyen Anh Tan <nguyenanhtan020203@gmail.com>
2025-05-18 22:23:30 +0700

    update

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// function openProject(url) {
-//     window.location.href = url;
-// }
-// //bấm vào link và nhảy sang trang web hiện tại//
+// Hàm mở dự án trong tab mới
 function openProject(url) {
     window.open(url, '_blank');
 }
-// Bấm vào link và mở 1 trang web mới cùng trang chủ // áp dụng nguyên tắc cái nào có trước thì sẽ áp dụng trước //
